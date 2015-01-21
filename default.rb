f870fa9c8965021ef7fd01c0191858aa6c55f076
shauncoon2409 <shauncoon2409@gmail.com>
2015-01-20 19:42:01 -0500

    default.rb still has no new docroot, but has a php file to share

--- default.rb
+++ default.rb
@@ -1,7 +1,17 @@
 directory(node)
 
 web_app(node) do
-  server_name(node)
   docroot(node)
   template('vhost.conf.erb')
+end
+
+file '/var/www/html/index.php' do
+  content '<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>'
 end
