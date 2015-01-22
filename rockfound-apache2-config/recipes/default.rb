242718b2f60074091b587ce8c477d8e7a36360b5
shauncoon2409 <shauncoon2409@gmail.com>
2015-01-22 16:25:22 -0500

    reenabling vhosts config, adding a directory on the end hosts

--- rockfound-apache2-config/recipes/default.rb
+++ rockfound-apache2-config/recipes/default.rb
@@ -5,10 +5,28 @@
 #  template('vhost.conf.erb')
 #end
 
-template "/var/www/html/index.php" do
+directory "/var/www/rockefeller/html" do
+  recursive true
+  owner "nobody"
+  mode "755"
+  action: create
+end
+
+
+web_app "rockefeller" do
+    template 'vhost.conf.erb'
+    server_name node['rockefeller']['staging.rock-public.ahundredyears.com']
+    docroot "/var/www/rockefeller/html"
+    allow_override 'All'
+    cookbook 'apache2'
+end
+
+
+template "/var/www/rockefeller/html/index.php" do
   source "index.php.erb"
   action :create_if_missing # see actions section below
 end
+
 
 #file '/var/www/html/index.php' do
 #  content '<html>
