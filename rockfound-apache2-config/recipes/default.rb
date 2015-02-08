e6f11e47214d1f37769bdff59b21b31d09a01cdc
shauncoon2409 <shauncoon2409@gmail.com>
2015-02-08 18:04:38 -0500

    swapped _value variables around

--- rockfound-apache2-config/recipes/default.rb
+++ rockfound-apache2-config/recipes/default.rb
@@ -67,12 +67,12 @@ template "/var/www/rockefeller/config/environments/local.php" do
   owner "nobody"
   mode "755"
     variables({
-       :db_host => node[:db_host_value],
-       :db_user => node[:db_user_value],
-       :db_password => node[:db_password_value],
-       :db_name => node[:db_name_value],
-       :wp_home => node[:wp_home_value],
-       :wp_siteurl => node[:wp_siteurl_value]
+       :db_host_value => node[:db_host],
+       :db_user_value => node[:db_user],
+       :db_password_value => node[:db_password],
+       :db_name_value => node[:db_name],
+       :wp_home_value => node[:wp_home],
+       :wp_siteurl_value => node[:wp_siteurl]
     })
 end  
 
